cc5457141153961cebce16633ce6b11584c9df19
Pravin <pravinpagar290@gmail.com>
2025-08-27 22:45:55 +0530

    tailwind setup

---
+++ src/Components/Footer.jsx
@@ -0,0 +1,8 @@
+import React from "react";
+
+export default function Footer()
+{
+    return(
+        <h1 className="bg-red-50">Footer</h1>
+    )
+}
---
+++ src/Components/Header.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+export default function Header()
+{
+    return(
+        <div>
+            <img src="https://svitnashik.in/Images/Header/Final_Svit_Header.png" alt="" />
+            <ul>
+                <li></li>
+            </ul>
+        </div>
+    )
+}
